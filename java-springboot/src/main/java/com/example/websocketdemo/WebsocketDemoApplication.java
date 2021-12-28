e7ed69f4161798c206f3ed5c319ff722b1d3ddd3
sheanaaron <s3808041@student.rmit.edu.au>
2021-12-28 11:03:24 +0800

    smol change

--- java-springboot/src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ java-springboot/src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -8,5 +8,6 @@ public class WebsocketDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketDemoApplication.class, args);
+		// adding a small change
 	}
 }
